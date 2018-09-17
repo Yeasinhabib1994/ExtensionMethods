55bec9c900bfbadc842d8554a62534a6eebd29cf
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-17 17:35:37 +0600

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a line you want to shorten:");
+            var post = Console.ReadLine();
+            Console.WriteLine("input number of words in shortened line: ");
+            var numberOfWords = Convert.ToInt32(Console.ReadLine());
+            var shortenedPost = post.Shorten(numberOfWords);
+
+            Console.WriteLine(shortenedPost);
+        }
+    }
+}
